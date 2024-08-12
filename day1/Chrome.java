96a0cf4074921a012648449da81ae3da01a0637b
prajan5 <64765230+prajan5@users.noreply.github.com>
2024-08-12 23:44:07 +0530

    Add files via upload
    day1

---
+++ day1/Chrome.java
@@ -0,0 +1,23 @@
+package week1.day1;
+
+public class Chrome {
+
+	public static void main(String[] args) {
+		
+		float version = 91.0f;
+		String developer = "Google";
+		boolean isBeta = false;
+		int releaseYear  = 2008;
+		char shortcutKey = 'C';
+		
+		System.out.println(
+				"Chrome Browser Version: "+version+"\n"+
+				"Chrome Developer: "+developer+"\n"+
+				"Beta Version: "+isBeta+"\n"+
+				"Released Year: "+releaseYear+"\n"+
+				"ShortCut to search chrome: "+shortcutKey);
+			
+			
+	}
+
+}
---
+++ day1/HelloTestLeaf.java
@@ -0,0 +1,11 @@
+package week1.day1;
+
+public class HelloTestLeaf {
+	//Type main+control+space for main method 
+	public static void main(String[] args) {
+	//Type syso+control_space for print statment
+		System.out.println(" Welcome to TestLeaf");
+		
+	}
+
+}
